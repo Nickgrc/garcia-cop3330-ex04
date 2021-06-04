ff38d973390857ce0185dbbcf58643311d972eaf
Nickolas Garcia <nickgrc@knights.ucf.edu>
2021-06-04 00:07:31 -0400

    exercise 4

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 /*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Nickolas Garcia
+ */
+
+/*
 Exercise 4 - Mad Lib
 
 Mad libs are a simple game where you create a story template with blanks for words.
